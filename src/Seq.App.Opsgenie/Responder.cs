2af6449df3853a0798e9c884da1df5d9811886e1
MattMofDoom <matt.marlor@outlook.com>
2021-06-09 16:24:15 +1000

    Handle User responder type

--- src/Seq.App.Opsgenie/Responder.cs
+++ src/Seq.App.Opsgenie/Responder.cs
@@ -1,7 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace Seq.App.Opsgenie
 {
     class Responder
     {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string Username { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string Name { get; set; }
         public ResponderType Type { get; set; }
     }
